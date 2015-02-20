53af22c77f4ca54208846f34d5867a18fc6a6abe
John <john@email.edu>
2015-02-20 11:41:46 -0500

    factorial 3

--- factorial.rb
+++ factorial.rb
@@ -1,11 +1,5 @@
 def factorial_of num
-  fact = 1
-  (1..num).each {|n| fact*=n}
-  fact
+  (1..num).inject(:*)
 end
 
-five = 5
-six = 6
-
-puts factorial_of five
-puts factorial_of six
+(5..6).each{|n| puts factorial_of n}
