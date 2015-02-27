e922313c15bb8496de0c4debdf71f7535ccda2a1
James <james@email.edu>
2015-02-27 13:33:25 -0500

    factorial 7 Add a method to compute recursively

--- factorial.rb
+++ factorial.rb
@@ -2,4 +2,8 @@ class Factorial
   def self.of num
     (1..num).inject(:*)
   end
+
+  def self.compute_recursively num
+    (num <= 1)? 1: num * Factorial.compute_recursively( num - 1 )
+  end
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
 require './factorial'
 
 (5..6).each{|n| puts Factorial.of n}
+
+puts Factorial.compute_recursively 6
+
 puts "end"
