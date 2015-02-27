8257e3dbf71ca61bf4c1687edb141e01042124f3
James <james@email.edu>
2015-02-27 13:11:09 -0500

    factorial 5

--- factorial.rb
+++ factorial.rb
@@ -1,6 +1,3 @@
 def factorial_of num
   (1..num).inject(:*)
 end
-
-(5..6).each{|n| puts factorial_of n}
-puts "end"
---
+++ test.rb
@@ -0,0 +1,4 @@
+require './factorial'
+
+(5..6).each{|n| puts factorial_of n}
+puts "end"
